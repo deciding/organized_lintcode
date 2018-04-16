57bccaa61cb8066f92523b1a286b71edb25e3be7
deciding <zhangzn710@gmail.com>
2018-04-16 18:12:00 +0800

    Create Maximum Gap.java

---
+++ hard/Maximum Gap.java
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param nums: an array of integers
+     * @return: the maximum difference
+     */
+    public int maximumGap(int[] nums) {
+        // write your code here
+        if(nums==null||nums.length==0||nums.length==1) return 0;
+        int n=nums.length;
+        int max=nums[0],min=nums[0];
+        for(int i=0;i<n;i++){
+            max=Math.max(max,nums[i]);
+            min=Math.min(min,nums[i]);
+        }
+        int range=(max-min)/(n-1);
+        if(range==0) range++;
+        int[] binMax=new int[n];
+        int[] binMin=new int[n];
+        for(int i=0;i<n;i++){
+            binMax[i]=Integer.MIN_VALUE;
+            binMin[i]=Integer.MAX_VALUE;
+        }
+        for(int i=0;i<n;i++){
+            int bin=(nums[i]-min)/range;
+            binMax[bin]=Math.max(binMax[bin],nums[i]);
+            binMin[bin]=Math.min(binMin[bin],nums[i]);
+        }
+        int prev=0;
+        int ans=0;
+        for(int i=1;i<n;i++)
+        if(binMax[i]!=Integer.MIN_VALUE){
+            ans=Math.max(ans,binMin[i]-binMax[prev]);
+            prev=i;
+        }
+        return ans;
+    }
+}

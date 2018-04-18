47672b82bcde80f0e4651a0faae1c09cdebc6dcd
deciding <zhangzn710@gmail.com>
2018-04-18 12:16:58 +0800

    Create H_paint house ii.java

---
+++ array/dp greedy/H_paint house ii.java
@@ -0,0 +1,27 @@
+public class Solution {
+    /**
+     * @param costs n x k cost matrix
+     * @return an integer, the minimum cost to paint all houses
+     */
+    public int minCostII(int[][] costs) {
+        // Write your code here
+        if(costs==null) return 0;
+        int n=costs.length;
+        if(n==0) return 0;
+        int k=costs[0].length;
+        if(k==0) return 0;
+        int[] leftMin=new int[k],rightMin=new int[k];
+        for(int i=0;i<n;i++){
+            if(i!=0)
+            for(int j=0;j<k;j++)
+                costs[i][j]+=Math.min(leftMin[j],rightMin[j]);
+            for(int x=0;x<k;x++)
+                if(x==0) leftMin[x]=Integer.MAX_VALUE;
+                else leftMin[x]=Math.min(leftMin[x-1],costs[i][x-1]);
+            for(int x=k-1;x>=0;x--)
+                if(x==k-1) rightMin[x]=Integer.MAX_VALUE;
+                else rightMin[x]=Math.min(rightMin[x+1],costs[i][x+1]);
+        }
+        return Math.min(leftMin[k-1],costs[n-1][k-1]);
+    }
+}
